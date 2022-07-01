f7e7ce2ea0f5f42f45ded885af94c96f48cb4ca5
adamauthor <ahmediarolzasov@gmail.com>
2022-07-01 17:02:33 +0600

    DiscordJS tutorial v0.3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 
-const generateImage = require('./generateimage')
+// const generateImage = require('./generateimage')
 const client = new Discord.Client({
     intents: [
         "GUILDS",
@@ -10,24 +10,41 @@ const client = new Discord.Client({
     ]
 })
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+let bot = {
+    client,
+    prefix: 'n.',
+    owners: '705350299031830569'
+}
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'hi') {
-        message.reply('Hello, Adam!')
-    }
-}) 
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-const welcomeChannelID = '991655148784975893'
+client.loadEvents = (bot, reload) => require('./handlers/events')(bot, reload)
+client.loadCommands  = (bot, reload) => require('./handlers/commands')(bot, reload)
 
-client.on('guildMemberAdd', async (member) => {
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChannelID).send({
-        content: `<@${member.id}> Welcome to the test server!`,
-        files: [img]
-    })
-})
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot 
+
+// client.on('ready', () => {
+//     console.log(`Logged in as ${client.user.tag}`)
+// })
+
+// client.on('messageCreate', (message) => {
+//     if (message.content === 'hi') {
+//         message.reply('Hello, Adam!')
+//     }
+// }) 
+
+// const welcomeChannelID = '991655148784975893'
+
+// client.on('guildMemberAdd', async (member) => {
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChannelID).send({
+//         content: `<@${member.id}> Welcome to the test server!`,
+//         files: [img]
+//     })
+// })
 
 client.login(process.env.TOKEN)
